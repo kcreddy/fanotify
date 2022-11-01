8ec08f7e27813f3aa3ce1cded3c99cdfd010816f
kcreddy <krish.reddy91@gmail.com>
2022-11-01 18:57:20 +0530

    logs

--- fanotify_trial.go
+++ fanotify_trial.go
@@ -135,6 +135,10 @@ func fileOrDirCreated() (uint, uint64) {
 // NOTE (Caveat) when the marked directory is deleted the event
 // file handle becomes stale and the event escapes
 func fileDeleteSelf() (uint, uint64) {
+	log.Printf("unix.FAN_CLASS_NOTIF: %v | unix.FD_CLOEXEC: %v | unix.FAN_REPORT_FID: %v", unix.FAN_CLASS_NOTIF, unix.FD_CLOEXEC, unix.FAN_REPORT_FID)
+	log.Printf("unix.FAN_DELETE: %v | unix.FAN_DELETE_SELF: %v | unix.FAN_ONDIR: %v", unix.FAN_DELETE, unix.FAN_DELETE_SELF, unix.FAN_ONDIR)
+	log.Printf("unix.FAN_CLASS_NOTIF | unix.FD_CLOEXEC | unix.FAN_REPORT_FID : %v", unix.FAN_CLASS_NOTIF|unix.FD_CLOEXEC|unix.FAN_REPORT_FID)
+	log.Printf("unix.FAN_DELETE | unix.FAN_DELETE_SELF | unix.FAN_ONDIR : %v", unix.FAN_DELETE|unix.FAN_DELETE_SELF|unix.FAN_ONDIR)
 	flags := uint(unix.FAN_CLASS_NOTIF | unix.FD_CLOEXEC | unix.FAN_REPORT_FID)
 	mask := uint64(unix.FAN_DELETE | unix.FAN_DELETE_SELF | unix.FAN_ONDIR)
 	return flags, mask
